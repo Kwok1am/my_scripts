469f1188905ba93d99be8f6e1a48c3622c87ce87
Kwok1am <51690055+Kwok1am@users.noreply.github.com>
2021-07-22 09:24:11 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,6 +4,8 @@
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
+#全民摸冰
+6 9,12 * * * node /scripts/jd_mbwenmoux.js  >> /scripts/logs/jd_mbwenmoux.log 2>&1
 #京东极速版红包(活动时间：2021-5-5至2021-5-31)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 
