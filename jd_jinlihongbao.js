2fdc59699e2f2ea52bd43ed21b3b11019b7e677b
GitHub Action <action@github.com>
2021-12-03 12:30:49 +0000

    .更新

---
+++ jd_jinlihongbao.js
@@ -0,0 +1,283 @@
+"use strict";
+/**
+ * 京东-锦鲤红包
+ * 做任务、助力、开红包
+ * cron: 2 0,6,18 * * *
+ * 固定log，不知道什么时候会gg
+ * CK1助力顺序
+ * HW_Priority: boolean
+ * true  HW.ts -> 内部
+ * false 内部   -> HW.ts
+ */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+exports.__esModule = true;
+var axios_1 = require("axios");
+var TS_USER_AGENTS_1 = require("./TS_USER_AGENTS");
+var cookie = '', res = '', UserName, index;
+var shareCodesSelf = [], shareCodes = [], shareCodesHW = [], HW_Priority = true;
+process.env.HW_Priority === 'false' ? HW_Priority = false : '';
+!(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var cookiesArr, i, e_1, i, _i, shareCodes_1, code, i, j, _a, _b, t, _c, _d, t, _e, _f, tp, e_2;
+    return __generator(this, function (_g) {
+        switch (_g.label) {
+            case 0: return [4 /*yield*/, (0, TS_USER_AGENTS_1.requireConfig)()];
+            case 1:
+                cookiesArr = _g.sent();
+                i = 0;
+                _g.label = 2;
+            case 2:
+                if (!(i < cookiesArr.length)) return [3 /*break*/, 10];
+                _g.label = 3;
+            case 3:
+                _g.trys.push([3, 8, , 9]);
+                cookie = cookiesArr[i];
+                UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1]);
+                index = i + 1;
+                console.log("\n\u5F00\u59CB\u3010\u4EAC\u4E1C\u8D26\u53F7".concat(index, "\u3011").concat(UserName, "\n"));
+                return [4 /*yield*/, api('h5launch', { "followShop": 0, "random": (0, TS_USER_AGENTS_1.getRandomNumberByRange)(36135846, 74613584), "log": "".concat(Date.now(), "~0iuxyee"), "sceneid": "JLHBhPageh5" })];
+            case 4:
+                res = _g.sent();
+                console.log('活动初始化：', res.data.result.statusDesc);
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 5:
+                _g.sent();
+                return [4 /*yield*/, api('h5activityIndex', { "isjdapp": 1 })];
+            case 6:
+                res = _g.sent();
+                console.log('红包ID：', res.data.result.redpacketInfo.id);
+                shareCodesSelf.push(res.data.result.redpacketInfo.id);
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 7:
+                _g.sent();
+                return [3 /*break*/, 9];
+            case 8:
+                e_1 = _g.sent();
+                console.log(e_1);
+                return [3 /*break*/, 9];
+            case 9:
+                i++;
+                return [3 /*break*/, 2];
+            case 10:
+                console.log('内部助力：', shareCodesSelf);
+                i = 0;
+                _g.label = 11;
+            case 11:
+                if (!(i < cookiesArr.length)) return [3 /*break*/, 19];
+                cookie = cookiesArr[i];
+                UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1]);
+                if (!(shareCodesHW.length === 0)) return [3 /*break*/, 13];
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.getshareCodeHW)('jlhb')];
+            case 12:
+                shareCodesHW = _g.sent();
+                _g.label = 13;
+            case 13:
+                if (i === 0 && HW_Priority) {
+                    shareCodes = Array.from(new Set(__spreadArray(__spreadArray([], shareCodesHW, true), shareCodesSelf, true)));
+                }
+                else {
+                    shareCodes = Array.from(new Set(__spreadArray(__spreadArray([], shareCodesSelf, true), shareCodesHW, true)));
+                }
+                _i = 0, shareCodes_1 = shareCodes;
+                _g.label = 14;
+            case 14:
+                if (!(_i < shareCodes_1.length)) return [3 /*break*/, 18];
+                code = shareCodes_1[_i];
+                console.log("\u8D26\u53F7 ".concat(UserName, " \u53BB\u52A9\u529B ").concat(code));
+                return [4 /*yield*/, api('jinli_h5assist', { "redPacketId": code, "followShop": 0, "random": (0, TS_USER_AGENTS_1.getRandomNumberByRange)(36135846, 74613584), "log": "".concat(Date.now(), "~0gga2ik"), "sceneid": "JLHBhPageh5" })];
+            case 15:
+                res = _g.sent();
+                if (res.data.result.status === 0) {
+                    console.log('助力成功：', parseFloat(res.data.result.assistReward.discount));
+                }
+                else if (res.data.result.status === 3) {
+                    console.log('今日助力次数已满');
+                    return [3 /*break*/, 18];
+                }
+                else {
+                    console.log('助力结果：', res.data.result.statusDesc);
+                }
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 16:
+                _g.sent();
+                _g.label = 17;
+            case 17:
+                _i++;
+                return [3 /*break*/, 14];
+            case 18:
+                i++;
+                return [3 /*break*/, 11];
+            case 19:
+                i = 0;
+                _g.label = 20;
+            case 20:
+                if (!(i < cookiesArr.length)) return [3 /*break*/, 47];
+                _g.label = 21;
+            case 21:
+                _g.trys.push([21, 45, , 46]);
+                cookie = cookiesArr[i];
+                UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1]);
+                index = i + 1;
+                console.log("\n\u5F00\u59CB\u3010\u4EAC\u4E1C\u8D26\u53F7".concat(index, "\u3011").concat(UserName, "\n"));
+                j = 1;
+                _a = 0, _b = res.data.result.redpacketConfigFillRewardInfo;
+                _g.label = 22;
+            case 22:
+                if (!(_a < _b.length)) return [3 /*break*/, 28];
+                t = _b[_a];
+                if (!(t.packetStatus === 2)) return [3 /*break*/, 23];
+                console.log("\u7EA2\u5305".concat(j, "\u5DF2\u62C6\u8FC7\uFF0C\u83B7\u5F97"), t.packetAmount);
+                return [3 /*break*/, 26];
+            case 23:
+                if (!(t.packetStatus === 1)) return [3 /*break*/, 26];
+                console.log("\u7EA2\u5305".concat(j, "\u53EF\u62C6"));
+                return [4 /*yield*/, api('h5receiveRedpacketAll', { "random": (0, TS_USER_AGENTS_1.getRandomNumberByRange)(36135846, 74613584), "log": "".concat(Date.now(), "~0suodw0"), "sceneid": "JLHBhPageh5" })];
+            case 24:
+                res = _g.sent();
+                console.log(res.data.biz_msg, parseFloat(res.data.result.discount));
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 25:
+                _g.sent();
+                _g.label = 26;
+            case 26:
+                j++;
+                _g.label = 27;
+            case 27:
+                _a++;
+                return [3 /*break*/, 22];
+            case 28: return [4 /*yield*/, api('taskHomePage', {})];
+            case 29:
+                res = _g.sent();
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 30:
+                _g.sent();
+                _c = 0, _d = res.data.result.taskInfos;
+                _g.label = 31;
+            case 31:
+                if (!(_c < _d.length)) return [3 /*break*/, 44];
+                t = _d[_c];
+                if (!(!t.alreadyReceivedCount || t.alreadyReceivedCount < t.requireCount)) return [3 /*break*/, 40];
+                if (![2, 3, 4, 5, 8].includes(t.taskType)) return [3 /*break*/, 40];
+                return [4 /*yield*/, api('startTask', { "taskType": t.taskType, "random": (0, TS_USER_AGENTS_1.getRandomNumberByRange)(36135846, 74613584), "log": "".concat(Date.now(), "~1orj8k3"), "sceneid": "JLHBhPageh5" })];
+            case 32:
+                res = _g.sent();
+                console.log(t.title, res.data.biz_msg);
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 33:
+                _g.sent();
+                return [4 /*yield*/, api('getTaskDetailForColor', { taskType: t.taskType })];
+            case 34:
+                res = _g.sent();
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 35:
+                _g.sent();
+                _e = 0, _f = res.data.result.advertDetails;
+                _g.label = 36;
+            case 36:
+                if (!(_e < _f.length)) return [3 /*break*/, 40];
+                tp = _f[_e];
+                if (!(tp.status === 0)) return [3 /*break*/, 39];
+                return [4 /*yield*/, api('taskReportForColor', { "taskType": t.taskType, "detailId": tp.id })];
+            case 37:
+                res = _g.sent();
+                console.log(t.title, tp.name, res.data.biz_msg);
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 38:
+                _g.sent();
+                _g.label = 39;
+            case 39:
+                _e++;
+                return [3 /*break*/, 36];
+            case 40:
+                if (!(t.innerStatus === 3)) return [3 /*break*/, 43];
+                return [4 /*yield*/, api('h5receiveRedpacketAll', { "taskType": t.taskType, "random": (0, TS_USER_AGENTS_1.getRandomNumberByRange)(36135846, 74613584), "log": "".concat(Date.now(), "~138q6w6"), "sceneid": "JLHBhPageh5" })];
+            case 41:
+                res = _g.sent();
+                console.log("".concat(t.title, " \u6253\u5F00\u6210\u529F\uFF0C\u83B7\u5F97"), parseFloat(res.data.result.discount));
+                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
+            case 42:
+                _g.sent();
+                _g.label = 43;
+            case 43:
+                _c++;
+                return [3 /*break*/, 31];
+            case 44: return [3 /*break*/, 46];
+            case 45:
+                e_2 = _g.sent();
+                console.log(e_2);
+                return [3 /*break*/, 46];
+            case 46:
+                i++;
+                return [3 /*break*/, 20];
+            case 47: return [2 /*return*/];
+        }
+    });
+}); })();
+function api(fn, body) {
+    return __awaiter(this, void 0, void 0, function () {
+        var data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, axios_1["default"].post("https://api.m.jd.com/api?appid=jinlihongbao&functionId=".concat(fn, "&loginType=2&client=jinlihongbao&t=").concat(Date.now(), "&clientVersion=10.2.4&osVersion=-1"), "body=".concat(encodeURIComponent(JSON.stringify(body))), {
+                        headers: {
+                            'Host': 'api.m.jd.com',
+                            'Origin': 'https://happy.m.jd.com',
+                            'Connection': 'keep-alive',
+                            'Accept': 'application/json, text/plain, */*',
+                            'User-Agent': TS_USER_AGENTS_1["default"],
+                            'Referer': 'https://happy.m.jd.com/',
+                            'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Cookie': cookie
+                        }
+                    })];
+                case 1:
+                    data = (_a.sent()).data;
+                    return [2 /*return*/, data];
+            }
+        });
+    });
+}
