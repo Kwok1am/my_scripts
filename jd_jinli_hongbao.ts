2811d91775185cf0600c8b7384db85afa1f3fd33
GitHub Action <action@github.com>
2022-03-22 18:23:50 +0000

    .更新

--- jd_jinli_hongbao.ts
+++ jd_jinli_hongbao.ts
@@ -2,17 +2,17 @@
  * 京东-锦鲤红包
  * 做任务、助力、开红包
  * cron: 1 0,6,18 * * *
- * CK1助力顺序
- * HW.ts -> 内部
+ * CK1     HW.ts -> 内部
+ * CK2～n  内部   -> HW.ts
  */
 
 import axios from 'axios';
-import {sendNotify} from './sendNotify'
-import USER_AGENT, {requireConfig, wait, getRandomNumberByRange, getshareCodeHW, o2s} from "./TS_USER_AGENTS";
+import {logs} from './utils/jinli_log';
+import {getRandomNumberByRange, getshareCodeHW, o2s, randomString, requireConfig, wait} from "./TS_USER_AGENTS";
 
-let cookie: string = '', res: any = '', UserName: string
+let cookie: string = '', res: any = '', UserName: string, UA: string = ''
 let shareCodesSelf: string[] = [], shareCodes: string[] = [], shareCodesHW: string[] = [], fullCode: string[] = []
-let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
+let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2], log: string = '', logIndex: number = 10
 
 !(async () => {
   let cookiesArr: string[] = await requireConfig(false);
@@ -21,17 +21,24 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
       cookie = value;
       UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
       console.log(`\n开始【京东账号${index + 1}】${UserName}\n`);
+      UA = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random() * 4 + 10)}.${Math.ceil(Math.random() * 4)};${randomString(40)}`
+      log = logs[getRandomNumberByRange(0, logs.length - 1)]
 
-      res = await api('h5launch', {"followShop": 0, "random": getRandomNumberByRange(36135846, 74613584), "log": `${Date.now()}~0iuxyee`, "sceneid": "JLHBhPageh5"})
+      let random = log.match(/"random":"(\d+)"/)[1], log1 = log.match(/"log":"(.*)"/)[1]
+      res = await api('h5launch', {"followShop": 0, "random": random, "log": log1, "sceneid": "JLHBhPageh5"})
       console.log('活动初始化：', res.data.result.statusDesc)
+      await wait(1000)
 
       res = await api('h5activityIndex', {"isjdapp": 1})
       console.log('红包ID：', res.data.result.redpacketInfo.id)
       shareCodesSelf.push(res.data.result.redpacketInfo.id)
+      await wait(1000)
     } catch (e) {
       console.log(e)
     }
   }
+
+  await wait(3000)
 
   console.log('内部助力：', shareCodesSelf)
   for (let [index, value] of cookiesArr.entries()) {
@@ -52,10 +59,17 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
       }
       for (let code of shareCodes) {
         if (!fullCode.includes(code)) {
+          UA = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random() * 4 + 10)}.${Math.ceil(Math.random() * 4)};${randomString(40)}`
+          log = logs[getRandomNumberByRange(0, logs.length - 1)]
+          let random = log.match(/"random":"(\d+)"/)[1], log1 = log.match(/"log":"(.*)"/)[1]
           console.log(`账号${index + 1} ${UserName} 去助力 ${code} ${shareCodesSelf.includes(code) ? '*内部*' : ''}`)
-          res = await api('jinli_h5assist', {"redPacketId": code, "followShop": 0, "random": getRandomNumberByRange(36135846, 74613584), "log": `${Date.now()}~0gga2ik`, "sceneid": "JLHBhPageh5"})
+
+          res = await api('jinli_h5assist', {"redPacketId": code, "followShop": 0, "random": random, "log": log1, "sceneid": "JLHBhPageh5"})
+          o2s(res, 'jinli_h5assist')
+
           if (res.data.result.status === 0) {
             console.log('助力成功：', parseFloat(res.data.result.assistReward.discount))
+            await wait(1000)
             break
           } else if (res.data.result.status === 3) {
             console.log('今日助力次数已满')
@@ -81,8 +95,11 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
       cookie = value
       UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
       console.log(`\n开始【京东账号${index + 1}】${UserName}\n`);
-
+      UA = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random() * 4 + 10)}.${Math.ceil(Math.random() * 4)};${randomString(40)}`
+      log = logs[getRandomNumberByRange(0, logs.length - 1)]
+      let random = log.match(/"random":"(\d+)"/)[1], log1 = log.match(/"log":"(.*)"/)[1]
       // 做任务
+      /*
       res = await api('taskHomePage', {})
       await wait(1000)
       for (let t of res.data.result.taskInfos) {
@@ -107,10 +124,11 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
           console.log(`${t.title} 打开成功，获得`, parseFloat(res.data.result.discount))
           if (!min.includes(parseFloat(res.data.result.discount)))
             await sendNotify(`锦鲤红包`, `账号${index + 1} ${UserName}\n${res.data.result.discount}`)
-          await wait( 1000)
+          await wait(1000)
         }
       }
       await wait(1000)
+
 
       // 打开任务红包
       res = await api('taskHomePage', {})
@@ -125,6 +143,7 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
         }
       }
       await wait(2000)
+       */
 
       // 打开助力红包
       let j: number = 1
@@ -134,7 +153,7 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
           console.log(`红包${j}已拆过，获得`, t.packetAmount)
         } else if (t.packetStatus === 1) {
           console.log(`红包${j}可拆`)
-          res = await api('h5receiveRedpacketAll', {"random": getRandomNumberByRange(36135846, 74613584), "log": `${Date.now()}~0suodw0`, "sceneid": "JLHBhPageh5"})
+          res = await api('h5receiveRedpacketAll', {"random": random, "log": log1, "sceneid": "JLHBhPageh5"})
           console.log(res.data.biz_msg, parseFloat(res.data.result.discount))
           await wait(1000)
         } else {
@@ -145,22 +164,28 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2]
     } catch (e) {
       console.log(e)
     }
+    await wait(1000)
   }
 })()
 
-async function api(fn: string, body: object) {
-  let {data} = await axios.post(`https://api.m.jd.com/api?appid=jinlihongbao&functionId=${fn}&loginType=2&client=jinlihongbao&t=${Date.now()}&clientVersion=10.2.4&osVersion=-1`, `body=${encodeURIComponent(JSON.stringify(body))}`, {
+async function api(fn: string, body: object, retry: number = 0) {
+  let {data} = await axios.post(`https://api.m.jd.com/api?appid=jinlihongbao&functionId=${fn}&loginType=2&client=jinlihongbao&clientVersion=10.2.4&osVersion=AndroidOS&d_brand=Xiaomi&d_model=Xiaomi`, `body=${encodeURIComponent(JSON.stringify(body))}`, {
     headers: {
-      'Host': 'api.m.jd.com',
-      'Origin': 'https://happy.m.jd.com',
-      'Connection': 'keep-alive',
-      'Accept': 'application/json, text/plain, */*',
-      'User-Agent': USER_AGENT,
-      'Referer': 'https://happy.m.jd.com/',
-      'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
+      "Cookie": cookie,
+      "origin": "https://h5.m.jd.com",
+      "referer": "https://h5.m.jd.com/babelDiy/Zeus/2NUvze9e1uWf4amBhe1AV6ynmSuH/index.html",
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Cookie': cookie
+      "X-Requested-With": "com.jingdong.app.mall",
+      "User-Agent": UA,
     }
   })
+  if (data.rtn_code === 403 && retry < 3) {
+    console.log('retry...')
+    await wait(1000)
+    log = logs[getRandomNumberByRange(0, logs.length - 1)]
+    body['random'] = log.match(/"random":"(\d+)"/)[1]
+    body['log'] = log.match(/"log":"(.*)"/)[1]
+    await api(fn, body, ++retry)
+  }
   return data
 }
