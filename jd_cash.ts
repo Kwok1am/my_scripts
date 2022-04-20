0a3e28e170a5716568d0cfd7c84721cf5bdaf627
GitHub Action <action@github.com>
2022-04-20 18:25:36 +0000

    .更新

--- jd_cash.ts
+++ jd_cash.ts
@@ -1,27 +1,24 @@
-import USER_AGENT, {getRandomNumberByRange, o2s, post, requireConfig, wait} from './TS_USER_AGENTS'
-import 'dotenv/config'
+import USER_AGENT, {post, requireConfig, wait} from './TS_USER_AGENTS'
+import {getSign} from "./test/sign";
 
 let cookie: string = '', res: any = '', data: any, UserName: string
-let message: string = '', pandaToken: string[] = process.env.PANDA_TOKEN ? process.env.PANDA_TOKEN.split('&') : []
 
 !(async () => {
   let cookiesArr: string[] = await requireConfig()
-  for (let [index, value] of cookiesArr.entries()) {
+  for (let [index, value] of Object.entries(cookiesArr)) {
     cookie = value
     UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
     console.log(`\n开始【京东账号${index + 1}】${UserName}\n`)
-    message += `【账号${index + 1}】  ${UserName}\n`
 
     res = await api('cash_homePage', {})
-
-    o2s(res)
     if (res.data.result.signedStatus !== 1) {
       console.log('今日未签到')
       data = await api('cash_sign', {"remind": 0, "inviteCode": "", "type": 0, "breakReward": 0})
       await wait(1000)
-      o2s(data, '签到成功')
+      console.log('签到成功')
     }
     res = await api('cash_homePage', {})
+
     await wait(1000)
     let type: number[] = [2, 4, 31, 16, 3, 5, 17, 21]
     let otherTaskNum = res.data.result.taskInfos.filter(item => !type.includes(item.type)).length
@@ -30,7 +27,6 @@ let message: string = '', pandaToken: string[] = process.env.PANDA_TOKEN ? proce
 
     for (let i = 0; i < 10; i++) {
       res = await api('cash_homePage', {})
-      o2s(res)
       if (res.data.result.taskInfos.filter(item => type.includes(item.type) && item.doTimes === item.times).length === taskNum) {
         console.log('任务全部完成')
         break
@@ -44,25 +40,19 @@ let message: string = '', pandaToken: string[] = process.env.PANDA_TOKEN ? proce
             console.log('任务完成', data.data.result.totalMoney ?? '')
             break
           } else {
-            o2s(data, '任务失败')
+            console.log('任务失败', JSON.stringify(data))
             break
           }
         }
       }
       await wait(2000)
     }
-    const fs = require('fs')
-    fs.writeFileSync('.env', 'PANDA_TOKEN=""\n')
   }
 })()
 
 async function api(fn: string, body: object) {
-  let sign = await post('https://api.jds.codes/jd/sign', {fn, body}, {'Authorization': `Bearer ${pandaToken[getRandomNumberByRange(0, pandaToken.length - 1)]}`})
-  if (!sign?.data?.sign) {
-    o2s(sign, 'getSign Error')
-    return {}
-  }
-  return await post(`https://api.m.jd.com/client.action?functionId=${fn}`, sign.data.sign, {
+  let sign = getSign(fn, body)
+  return await post(`https://api.m.jd.com/client.action?functionId=${fn}`, sign, {
     'Host': 'api.m.jd.com',
     'Cookie': cookie,
     'content-type': 'application/x-www-form-urlencoded',
